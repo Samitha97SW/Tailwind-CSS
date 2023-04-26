db1e8506b7eeb1ca44b8dd86dfe8b058fe086a1f
Samitha97SW <samithasand123@gmail.com>
2023-04-26 17:09:40 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
+  
